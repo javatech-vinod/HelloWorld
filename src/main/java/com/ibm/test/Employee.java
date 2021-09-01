254ec555e31d11c0c568aef1b4ed33522395ad70
002A4W744 <002A4W744@WIN-ETP5VH96AUL>
2021-09-01 18:47:53 +0530

    commit

---
+++ src/main/java/com/ibm/test/Employee.java
@@ -0,0 +1,18 @@
+package com.ibm.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Employee {
+	private Integer id;
+	private String name;
+
+}
